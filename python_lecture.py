18cbbdbd4f8156540cee2779267a5d0bc10ecd27
Eric Smith <erwsmith@gmail.com>
2022-07-06 14:42:46 -0600

    initial

---
+++ python_lecture.py
@@ -0,0 +1,71 @@
+# a = 28
+# b = 1.5
+# c = "hello!"
+# d = True
+
+# NoneType
+# e = None 
+
+# name = input("Name: ")
+# print(f"hello, {name}!")
+
+# try: 
+#     if float(input("Enter a number: ")) > 0:
+#         print("positive")
+#     else:
+#         print("not positive")
+# except:
+#     print("Enter a number")
+
+# name = "Harry"
+# print(name[0])
+
+# names = ["Harry", "Ron", "Hermione"]
+# print(names[0][-1])
+
+# tuple (immutable)
+# coordinate = (10.0, 20.0)
+
+# tuple unpacking
+# a, b = coordinate
+# print(a, b)
+
+# names = ["Harry", "Ron", "Hermione", "Ginny"]
+# names.append("Draco")
+# names.sort()
+# print(names)
+
+# create a set
+# s = set()
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(3)
+# s.add(4)
+# s.remove(3)
+
+# print(s)
+# print(f"the set has {len(s)} elements")
+
+# for i in [0, 1, 2, 3, 4, 5]:
+#     print(i)
+
+# for i in range(6):
+#     print(i)
+
+# names = ["Harry", "Ron", "Hermione", "Ginny"]
+# for name in names:
+#     print(name)
+
+# name = "Eric"
+# for char in name:
+#     print(char)
+
+# houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+# houses["Hermione"] = "Gryffindor"
+# print(houses["Harry"], houses["Hermione"])
+
+def square(x):
+    return x * x
+
+
