93ff8a43abe83ec96f394ad7826725692b0f1259
Eric Smith <erwsmith@gmail.com>
2022-07-13 13:01:30 -0600

    add tasks with form done

--- w3-django/lecture3/tasks/views.py
+++ w3-django/lecture3/tasks/views.py
@@ -1,13 +1,37 @@
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
 
-tasks = ["foo", "bar", "baz", "bing"]
+tasks = []
 
-# Create your views here.
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=3)
+
+
+# VIEWS
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
 
+
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            # get data from form and add to tasks list
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
