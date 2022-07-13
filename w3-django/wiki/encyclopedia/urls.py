5c6401e41bbe328477d539d57bb9550e6b9b630a
Eric Smith <erwsmith@gmail.com>
2022-07-13 14:58:51 -0600

    kind of working

--- w3-django/wiki/encyclopedia/urls.py
+++ w3-django/wiki/encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("create", views.index, name="create"),
+    path("edit", views.index, name="edit")
 ]
--- w3-django/wiki/encyclopedia/views.py
+++ w3-django/wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+# import markdown2
 
+from django.shortcuts import render
 from . import util
 
 
@@ -8,3 +9,27 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def create(request):
+    return render(request, "encyclopedia/create.html")
+
+
+def edit(request):
+    return render(request, "encyclopedia/edit.html")
+
+
+# def random(request):
+#     return render(request, "encyclopedia/TODO.html")
+
+
+# TODO Convert from markdown to HTML
+# print(markdown2.markdown("*boo!*"))
+# or use `html = markdown_path(PATH)`
+    # output: u'<p><em>boo!</em></p>\n'
+
+# markdowner = Markdown()
+# markdowner.convert("*boo!*")
+    # output: u'<p><em>boo!</em></p>\n'
+
+# markdowner.convert("**boom!**")
+    # output: u'<p><strong>boom!</strong></p>\n'
