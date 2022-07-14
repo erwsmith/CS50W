1d4070c6f18970c7ad22da5a76ccd7418dbf460d
Eric Smith <erwsmith@gmail.com>
2022-07-14 11:41:09 -0600

    working search, still need to return entries that have query as substring

--- w3-django/wiki/encyclopedia/views.py
+++ w3-django/wiki/encyclopedia/views.py
@@ -14,44 +14,65 @@ class NewSearchForm(forms.Form):
 # VIEWS
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewSearchForm()
     })
 
 
 def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "title": title,
-        "entry": markdown2.markdown(util.get_entry(title))
+        "entry": markdown2.markdown(util.get_entry(title)),
+        "form": NewSearchForm()
     })
 
 
 def search(request):
-    query = "query placeholder"
-    return render(request, "encyclopedia/search.html", {
-        "query": query
+    query = "query not set"
+
+    # get current list of entries
+    entries = util.list_entries()
+
+    # convert all entries in list to lowercase
+    entries = [entry.lower() for entry in entries]
+
+    # get search query 
+    if request.method == "POST":
+        form = NewSearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data["query"].lower()
+
+            # check if the query exists
+            if query in entries:
+                return render(request, "encyclopedia/search.html", {
+                    "query": query,
+                    "form": NewSearchForm(),
+                    "entry": markdown2.markdown(util.get_entry(query))
+                })
+            else:
+                return render(request, "encyclopedia/index.html", {
+                    "entries": util.list_entries(),
+                    "form": NewSearchForm(),
+                })
+
+        else:
+            query = "form is not valid"
+            return render(request, "encyclopedia/index.html", {
+                "form": NewSearchForm()
+            })
+    
+    return render(request, "encyclopedia/index.html", {
+        "form": NewSearchForm()
     })
 
 
 def create(request):
-    return render(request, "encyclopedia/create.html")
+    return render(request, "encyclopedia/create.html", {
+        "form": NewSearchForm()
+    })
 
 
 def edit(request):
-    return render(request, "encyclopedia/edit.html")
-
-
-# def random(request):
-#     return render(request, "encyclopedia/TODO.html")
-
-
-# TODO Convert from markdown to HTML
-# print(markdown2.markdown("*boo!*"))
-# or use `html = markdown_path(PATH)`
-    # output: u'<p><em>boo!</em></p>\n'
-
-# markdowner = Markdown()
-# markdowner.convert("*boo!*")
-    # output: u'<p><em>boo!</em></p>\n'
-
-# markdowner.convert("**boom!**")
-    # output: u'<p><strong>boom!</strong></p>\n'
+    return render(request, "encyclopedia/edit.html", {
+        "form": NewSearchForm()
+    })
