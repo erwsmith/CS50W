aa27e604dad491c0955dc24e51eb25e0c6c86f3d
Eric Smith <erwsmith@gmail.com>
2022-07-15 14:28:25 -0600

    add randomPage (not 'random'!)

--- w3-django/wiki/encyclopedia/urls.py
+++ w3-django/wiki/encyclopedia/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
     path("create/", views.create, name="create"),
+    path("randomPage/", views.randomPage, name="randomPage"),
     path("edit/<str:title>", views.edit, name="edit")
 ]
