751286d072ced06b4bcf6ac840234a4bc90200aa
Eric Smith <erwsmith@gmail.com>
2022-07-20 14:09:34 -0600

    working Flight

--- w4-sql/airline/flights/models.py
+++ w4-sql/airline/flights/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# class called flight, inherits from Model
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    # def __str__(self):
+    #     return f"{self.origin} to {self.destination}, {self.duration}"
+
+
+# class Airport(models.Model):
+#     code = models.ForeignKey(max_length=3, )
+#     city = models.ForeignKey(max_length=64, )
+
+#     def __str__(self):
+#         return f"{self.city} ({self.code})"
