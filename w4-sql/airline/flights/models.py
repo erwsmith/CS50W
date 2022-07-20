0776c1b431a7093d22b7c05fa4eee3aa0a5208fa
Eric Smith <erwsmith@gmail.com>
2022-07-20 14:14:58 -0600

    add notes on usage

--- w4-sql/airline/flights/models.py
+++ w4-sql/airline/flights/models.py
@@ -9,6 +9,28 @@ class Flight(models.Model):
     # def __str__(self):
     #     return f"{self.origin} to {self.destination}, {self.duration}"
 
+# NOTE: This is how to use and test the Flight model
+# These 2 lines on initial creation/update only: 
+# $ python3 manage.py makemigrations
+# $ python3 manage.py migrate
+
+# After creation/update: 
+# $ python3 manage.py shell
+# >>> from flights.models import Flight
+# >>> f = Flight(origin="New York", destination="London", duration=415)
+# >>> f.save()
+# >>> f
+# <Flight: Flight object (1)>
+# >>> f.origin
+# 'New York'
+# >>> f.destination
+# 'London'
+# >>> f.duration
+# 415
+# >>> Flight.objects.all()
+# <QuerySet [<Flight: Flight object (1)>]>
+
+
 
 # class Airport(models.Model):
 #     code = models.ForeignKey(max_length=3, )
