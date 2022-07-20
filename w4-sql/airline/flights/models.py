364dde168221767a163899c1eb55c3788ba31aa9
Eric Smith <erwsmith@gmail.com>
2022-07-20 17:02:02 -0600

    views construction

--- w4-sql/airline/flights/models.py
+++ w4-sql/airline/flights/models.py
@@ -18,6 +18,17 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
 
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
+
+# NOTE: each model must be added to admin.py
+
 # NOTE: This is how to use and test the Flight model
 # These 2 lines on initial creation/update only: 
 # $ python3 manage.py makemigrations
--- w4-sql/airline/flights/views.py
+++ w4-sql/airline/flights/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .models import Flight, Passenger
+
+
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    })
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        
+        # get flight object
+        flight = Flight.objects.get(pk=flight_id)
+        
+        # get passenger object with passenger integer
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        
+        # add row to table for passenger's new flight booking
+        passenger.flights.add(flight)
+
+        # redirect to 'flight' page, sending flight.id as arg
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
