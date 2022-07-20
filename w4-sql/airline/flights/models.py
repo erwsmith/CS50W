18725fdd595a902dff108985d4d95f6d1a33c613
Eric Smith <erwsmith@gmail.com>
2022-07-20 16:06:52 -0600

    working models

--- w4-sql/airline/flights/models.py
+++ w4-sql/airline/flights/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
 # class called flight, inherits from Model
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"{self.origin} to {self.destination}, {self.duration}"
+        return f"{self.id}: {self.origin} to {self.destination}"
 
 # NOTE: This is how to use and test the Flight model
 # These 2 lines on initial creation/update only: 
@@ -29,12 +38,3 @@ class Flight(models.Model):
 # 415
 # >>> Flight.objects.all()
 # <QuerySet [<Flight: Flight object (1)>]>
-
-
-
-# class Airport(models.Model):
-#     code = models.ForeignKey(max_length=3, )
-#     city = models.ForeignKey(max_length=64, )
-
-#     def __str__(self):
-#         return f"{self.city} ({self.code})"
