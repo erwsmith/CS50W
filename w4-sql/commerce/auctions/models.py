da833c6095af94f7252fbeaf9e70a4ddcd5b294e
Eric Smith <erwsmith@gmail.com>
2022-07-22 16:12:39 -0600

    add Bid & Comment, allow some blanks in Listing

--- w4-sql/commerce/auctions/models.py
+++ w4-sql/commerce/auctions/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    listing_owner = models.ForeignKey(
+    user = models.ForeignKey(
         User,
         models.SET_NULL,
         null=True,
@@ -15,17 +15,46 @@ class Listing(models.Model):
     listing_title = models.CharField(max_length=100)
     description = models.CharField(max_length=10000)
     starting_bid = models.DecimalField(decimal_places=2, max_digits=12)
-    image_url = models.URLField(max_length=2048)
-    category = models.CharField(max_length=100)
+    image_url = models.URLField(max_length=2048, blank=True)
+    category = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return f"{self.id}: {self.listing_title}"
+        return f"{self.id}"
 
-# class Bids():
-#     pass
 
-# class Comments():
-#     pass
+class Bid(models.Model):
+    user = models.ForeignKey(
+        User,
+        models.SET_NULL,
+        null=True,
+        related_name="bidder",
+        )
+    listing = models.ForeignKey(
+        Listing,
+        models.SET_NULL,
+        null=True,
+        related_name="bid_listing_id",
+        )
+    bid = models.DecimalField(decimal_places=2, max_digits=12)
 
-# class Categories():
-#     pass 
+    def __str__(self):
+        return f"{self.id}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User,
+        models.SET_NULL,
+        null=True,
+        related_name="commenter",
+        )
+    listing = models.ForeignKey(
+        Listing,
+        models.SET_NULL,
+        null=True,
+        related_name="comment_listing_id",
+        )
+    comment = models.CharField(max_length=75)
+
+    def __str__(self):
+        return f"{self.id}"
