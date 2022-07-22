6bfbf87a851e4f7ae033df10dc6e9fb9747676ba
Eric Smith <erwsmith@gmail.com>
2022-07-22 11:37:34 -0600

    add required = bool for each field

--- w4-sql/commerce/auctions/forms.py
+++ w4-sql/commerce/auctions/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class CreateEntryForm(forms.Form):
-    listing_title = forms.CharField(label="Title", max_length=100)
-    description = forms.CharField(label="description", max_length=10000)
-    starting_bid = forms.DecimalField(label="starting_bid", decimal_places=2, max_digits=12)
-    image_url = forms.URLField(label="image_url", max_length=2048)
-    category = forms.CharField(label="category", max_length=100)
+    listing_title = forms.CharField(label="Title", max_length=100, required=True)
+    description = forms.CharField(label="description", max_length=10000, required=True)
+    starting_bid = forms.DecimalField(label="starting_bid", decimal_places=2, max_digits=12, required=True)
+    image_url = forms.URLField(label="image_url", max_length=2048, required=False)
+    category = forms.CharField(label="category", max_length=100, required=False)
