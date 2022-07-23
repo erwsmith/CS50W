9a21c5e3d7a6abf009e425490045a95d8d99a0ff
Eric Smith <erwsmith@gmail.com>
2022-07-23 16:33:08 -0600

    formatting + comment

--- w4-sql/commerce/auctions/admin.py
+++ w4-sql/commerce/auctions/admin.py
@@ -3,20 +3,18 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User, Listing, Bid, Comment
 
-
+# Django Admin Settings
 class ListingAdmin(admin.ModelAdmin):
     list_display = ("listing_title", "user", "description", "starting_bid", "category", "id") 
 
-
 class BidAdmin(admin.ModelAdmin):
     list_display = ("user", "listing", "bid", "id")
-
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("comment", "user", "listing", "id")
 
 
-# Register your models here.
+# Model registration
 admin.site.register(User, UserAdmin)
 admin.site.register(Listing, ListingAdmin)
 admin.site.register(Bid, BidAdmin)
