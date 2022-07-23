29e87770a9d26edc800f5f05929abe3c28c6568b
Eric Smith <erwsmith@gmail.com>
2022-07-23 16:20:41 -0600

    add slash to end of each path

--- w4-sql/commerce/auctions/urls.py
+++ w4-sql/commerce/auctions/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from . import views
 
+# URL Conf
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("watchlist", views.watchlist, name="watchlist"),
-    path("categories", views.categories, name="categories"),
-    path("<str:username>/create_listing", views.create_listing, name="create_listing"),
-    path("<int:listing_id>", views.listing, name="listing")
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("watchlist/", views.watchlist, name="watchlist"),
+    path("categories/", views.categories, name="categories"),
+    path("<str:username>/create_listing/", views.create_listing, name="create_listing"),
+    path("<int:listing_id>/", views.listing, name="listing")
 ]
