7510eb80d7d8a1b6e47c7b4b0c77d084c7c38424
Eric Smith <erwsmith@gmail.com>
2022-07-25 16:03:08 -0600

    change labels to more human-readable

--- w4-sql/commerce/auctions/forms.py
+++ w4-sql/commerce/auctions/forms.py
@@ -4,14 +4,14 @@ from .models import Category
 
 class CreateEntryForm(forms.Form):
     listing_title = forms.CharField(label="Title", max_length=100, required=True)
-    description = forms.CharField(label="description", max_length=10000, required=True)
-    starting_bid = forms.DecimalField(label="starting_bid", decimal_places=2, max_digits=12, required=True)
-    image_url = forms.URLField(label="image_url", max_length=2048, required=False)
+    description = forms.CharField(label="Description", max_length=10000, required=True)
+    starting_bid = forms.DecimalField(label="Starting Bid", decimal_places=2, max_digits=12, required=True)
+    image_url = forms.URLField(label="Image URL", max_length=2048, required=False)
     category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Select Category", required=False)
 
 
 class BidForm(forms.Form):
-    bid = forms.DecimalField(label="bid", decimal_places=2, max_digits=12, required=True)
+    bid = forms.DecimalField(label="", decimal_places=2, max_digits=12, required=True, min_value=0)
 
 
 # class Watchlist(forms.Form):
