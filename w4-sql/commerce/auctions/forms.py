ea36f6e0d74a06280489739dbc31ddb0dd7f32ec
Eric Smith <erwsmith@gmail.com>
2022-07-25 13:04:07 -0600

    add select from categories

--- w4-sql/commerce/auctions/forms.py
+++ w4-sql/commerce/auctions/forms.py
@@ -1,13 +1,12 @@
 from django import forms
 
-from .models import Category
 
 class CreateEntryForm(forms.Form):
     listing_title = forms.CharField(label="Title", max_length=100, required=True)
     description = forms.CharField(label="description", max_length=10000, required=True)
     starting_bid = forms.DecimalField(label="starting_bid", decimal_places=2, max_digits=12, required=True)
     image_url = forms.URLField(label="image_url", max_length=2048, required=False)
-    category = forms.ChoiceField(label="category", required=False, choices=Category.objects.all())
+    category = forms.CharField(label="category", max_length=100, required=False)
 
 
 class BidForm(forms.Form):
