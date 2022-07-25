c8751bf7ae31da3f8e295ac94d90826b25a37513
Eric Smith <erwsmith@gmail.com>
2022-07-25 10:27:38 -0600

    add category

--- w4-sql/commerce/auctions/urls.py
+++ w4-sql/commerce/auctions/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("watchlist/", views.watchlist, name="watchlist"),
     path("categories/", views.categories, name="categories"),
+    path("<str:category>", views.category, name="category"),
     path("<str:username>/create_listing/", views.create_listing, name="create_listing"),
-    path("<int:listing_id>/", views.listing, name="listing")
+    path("<int:listing_id>/", views.listing, name="listing"), 
 ]
