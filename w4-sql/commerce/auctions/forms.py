6adefe39a3e5122cf134c1a452a35f3161410888
Eric Smith <erwsmith@gmail.com>
2022-07-25 14:22:01 -0600

    change to ModelChoiceField

--- w4-sql/commerce/auctions/forms.py
+++ w4-sql/commerce/auctions/forms.py
@@ -7,7 +7,7 @@ class CreateEntryForm(forms.Form):
     description = forms.CharField(label="description", max_length=10000, required=True)
     starting_bid = forms.DecimalField(label="starting_bid", decimal_places=2, max_digits=12, required=True)
     image_url = forms.URLField(label="image_url", max_length=2048, required=False)
-    category = forms.ChoiceField(label="category", required=False, choices=Category.objects.values("category_name"))
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Select Category")
 
 
 class BidForm(forms.Form):
