a2973de1bd251c5ac972efa3b46162de7578cff2
Eric Smith <erwsmith@gmail.com>
2022-07-26 14:00:14 -0600

    register Watchlist & add WatchlistAdmin

--- w4-sql/commerce/auctions/admin.py
+++ w4-sql/commerce/auctions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Listing, Bid, Comment, Category
+from .models import User, Listing, Bid, Comment, Category, Watchlist
 
 # Django Admin Settings
 class ListingAdmin(admin.ModelAdmin):
@@ -18,6 +18,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("category_name", "id")
     ordering = ["category_name"]
 
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ("user", "id")
+    ordering = ["user"]
+    filter_horizontal = ("listings",)
+
 
 # Model registration
 admin.site.register(User, UserAdmin)
@@ -25,3 +30,4 @@ admin.site.register(Listing, ListingAdmin)
 admin.site.register(Bid, BidAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Watchlist, WatchlistAdmin)
