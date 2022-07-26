4f9c625c8a6e85278b73f88f9907fd58a7d72545
Eric Smith <erwsmith@gmail.com>
2022-07-26 09:56:36 -0600

    change from category to category_id

--- w4-sql/commerce/auctions/urls.py
+++ w4-sql/commerce/auctions/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("watchlist/", views.watchlist, name="watchlist"),
     path("categories/", views.categories, name="categories"),
-    path("<str:category>", views.category, name="category"),
+    path("<str:category_id>/", views.category_view, name="category_view"),
     path("<str:username>/create_listing/", views.create_listing, name="create_listing"),
     path("<int:listing_id>/", views.listing, name="listing"), 
 ]
