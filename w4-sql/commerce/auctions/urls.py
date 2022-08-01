5d1e904d4915d2ad4b720513e0ba2af3591f2756
Eric Smith <erwsmith@gmail.com>
2022-08-01 16:32:01 -0600

    change category_id to int

--- w4-sql/commerce/auctions/urls.py
+++ w4-sql/commerce/auctions/urls.py
@@ -5,13 +5,13 @@ from . import views
 # URL Conf
 urlpatterns = [
     path("", views.index, name="index"),
-    path("closed_listings/", views.closed_listings, name="closed_listings"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
     path("categories/", views.categories, name="categories"),
-    path("<str:category_id>/", views.category_view, name="category_view"),
-    path("<str:username>/create_listing/", views.create_listing, name="create_listing"),
-    path("<int:listing_id>/listing_view", views.listing_view, name="listing_view"), 
     path("watchlist_view", views.watchlist_view, name="watchlist_view"),
+    path("<int:category_id>/", views.category_view, name="category_view"),
+    path("closed_listings/", views.closed_listings, name="closed_listings"),
+    path("<int:listing_id>/listing_view", views.listing_view, name="listing_view"), 
+    path("<str:username>/create_listing/", views.create_listing, name="create_listing"),
 ]
