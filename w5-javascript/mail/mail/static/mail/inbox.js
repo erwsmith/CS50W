aecc5d60c0c8bfe2768773d9335037d37f2da760
Eric Smith <erwsmith@gmail.com>
2022-08-03 13:52:45 -0600

    add comments

--- w5-javascript/mail/mail/static/mail/inbox.js
+++ w5-javascript/mail/mail/static/mail/inbox.js
@@ -23,11 +23,62 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  // document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#emails-view').innerHTML = `<h3>This is the ${mailbox} mailbox.</h3>`;
 }
+
+// // Create new HTML element 
+  // const element = document.createElement('div');
+  // element.innerHTML = 'This is the content of the div.';
+  // element.addEventListener('click', function() {
+  //     console.log('This element has been clicked!')
+  // });
+  // document.querySelector('#container').append(element);
+
+// // Get all emails in 'mailbox'
+  // fetch('/emails/${mailbox}')
+  // .then(response => response.json())
+  // .then(emails => {
+  //     console.log(emails);
+  //     document.querySelector('#emails-view').innerHTML = `${emails}`;
+  // });
+
+// // GET /emails/<int:email_id>
+// // get email with id=100
+// fetch('/emails/${email.id}')
+// .then(response => response.json())
+// .then(email => {
+//     // Print email
+//     console.log(email);
+
+//     // ... do something else with email ...
+// });
+
+// // POST /emails
+// fetch('/emails', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//       recipients: 'baz@example.com',
+//       subject: 'Meeting time',
+//       body: 'How about we meet tomorrow at 3pm?'
+//   })
+// })
+// .then(response => response.json())
+// .then(result => {
+//     // Print result
+//     console.log(result);
+// });
+
+// // PUT /emails/<int:email_id>
+// fetch('/emails/100', {
+//   method: 'PUT',
+//   body: JSON.stringify({
+//       archived: true // or read: true / false
+//   })
+// })
