9df79d24d9605f506ccaa6573c047804f25f6013
Eric Smith <erwsmith@gmail.com>
2022-08-03 13:52:20 -0600

    register models

--- w5-javascript/mail/mail/admin.py
+++ w5-javascript/mail/mail/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User, Email
+
+# Django Admin Settings
+class EmailAdmin(admin.ModelAdmin):
+    # list_display = ("user", "sender", "recipients", "subject", "timestamp", "read", "archived", "id")
+    ordering = ["user"]
+
+
+# Model registration
+admin.site.register(User, UserAdmin)
+admin.site.register(Email, EmailAdmin)
