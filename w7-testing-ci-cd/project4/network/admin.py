b54c6cff754ffcfbf60daaab6cd5e70f540b98de
Eric Smith <erwsmith@gmail.com>
2022-08-08 09:34:03 -0600

    registered User

--- w7-testing-ci-cd/project4/network/admin.py
+++ w7-testing-ci-cd/project4/network/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+# # Django Admin Settings
+# class EmailAdmin(admin.ModelAdmin):
+#     list_display = ("user", "sender", "subject", "timestamp", "read", "archived", "id")
+#     ordering = ["user"]
+
+# Model registration
+admin.site.register(User, UserAdmin)
