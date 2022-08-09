7db33cb0225a35f616378791b4aeccfdd2db0130
Eric Smith <erwsmith@gmail.com>
2022-08-08 19:29:40 -0600

    register Post, Like, Follower

--- w7-testing-ci-cd/project4/network/admin.py
+++ w7-testing-ci-cd/project4/network/admin.py
@@ -2,12 +2,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Post, Like, Follower
 
 # Django Admin Settings
-# class EmailAdmin(admin.ModelAdmin):
-#     list_display = ("user", "sender", "subject", "timestamp", "read", "archived", "id")
-#     ordering = ["user"]
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("user", "body", "timestamp", "id")
+    ordering = ["user"]
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "liked", "id")
+    ordering = ["user"]
+
+class FollowerAdmin(admin.ModelAdmin):
+    list_display = ("user", "id")
+    ordering = ["user"]
 
 # Model registration
 admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Follower, FollowerAdmin)
