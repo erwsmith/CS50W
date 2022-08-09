2952f58b04155e78f801817672e6cb1aee8dc8fd
Eric Smith <erwsmith@gmail.com>
2022-08-08 19:49:52 -0600

    fleshed out a bit

--- w7-testing-ci-cd/project4/network/tests.py
+++ w7-testing-ci-cd/project4/network/tests.py
@@ -10,6 +10,7 @@ class NetworkTestCase(TestCase):
         # create users
         u1 = User.objects.create(username="erwsmith", email="erwsmith@gmail.com")
         u2 = User.objects.create(username="user2", email="user2@gmail.com")
+        u3 = User.objects.create(username="user3", email="user3@gmail.com")
         # create posts
         p1 = Post.objects.create(user=u1, body="Post A", id=1)
         p2 = Post.objects.create(user=u2, body="Post B", id=2)
@@ -17,11 +18,14 @@ class NetworkTestCase(TestCase):
         Like.objects.create(post=p1, user=u1, liked=True)
         Like.objects.create(post=p2, user=u2, liked=True)
         Like.objects.create(post=p2, user=u1, liked=True)
+        Like.objects.create(post=p2, user=u3, liked=True)
         # create followers
         f1 = Follower.objects.create(user=u1) 
         f1.following.add(u2)
         f2 = Follower.objects.create(user=u2)
         f2.following.add(u1)
+        f3 = Follower.objects.create(user=u3)
+        f3.following.add(u1)
 
     def test_likes_count_1(self):
         p = Post.objects.get(pk=1)
@@ -29,27 +33,19 @@ class NetworkTestCase(TestCase):
     
     def test_likes_count_2(self):
         p = Post.objects.get(pk=2)
-        self.assertEqual(p.likes.count(), 2)
+        self.assertEqual(p.likes.count(), 3)
 
-    def followers_count(self):
-        p = User.objects.get(username="erwsmith")
-        self.assertEqual(p.followers.count(), 1)
+    def test_followers_count(self):
+        u = User.objects.get(pk=1)
+        self.assertEqual(u.followers.count(), 2)
 
-    # def test_valid_flight(self):
-    #     a1 = Airport.objects.get(code="AAA")
-    #     a2 = Airport.objects.get(code="BBB")
-    #     f = Flight.objects.get(origin=a1, destination=a2, duration=100)
+    def test_valid_follower(self):
+        u1 = User.objects.get(username="erwsmith")
+        f = Follower.objects.get(user=u1)
+        self.assertTrue(f.is_valid_follower())
+
     #     self.assertTrue(f.is_valid_flight())
     
-    # def test_invalid_flight_destination(self):
-    #     a1 = Airport.objects.get(code="AAA")
-    #     f = Flight.objects.get(origin=a1, destination=a1)
-    #     self.assertFalse(f.is_valid_flight())
-
-    # def test_invalid_flight_duration(self):
-    #     a1 = Airport.objects.get(code="AAA")
-    #     a2 = Airport.objects.get(code="BBB")
-    #     f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
     #     self.assertFalse(f.is_valid_flight())
 
     # def test_index(self):
