09fef2ce2930fd2a69e226bf058ec48e8c6ecf94
Eric Smith <erwsmith@gmail.com>
2022-08-08 18:44:51 -0600

    changes pre-migration

--- w7-testing-ci-cd/project4/network/models.py
+++ w7-testing-ci-cd/project4/network/models.py
@@ -5,35 +5,25 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-
 class Post(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     body = models.TextField(blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
-    likers = models.ManyToManyField("User", related_name="likedPosts")
 
-    # def serialize(self):
-    #     return {
-    #         "id": self.id,
-    #         "user": self.user.username,
-    #         "poster": self.poster.username,
-    #         "likers": [user.email for user in self.likers.all()],
-    #         "body": self.body,
-    #         "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
-    #     }
+    def __str__(self):
+        return f"{self.id}"
 
-# class Like(models.Model):
-#     user = models.ForeignKey(User, models.SET_NULL, null=True)
-#     post = models.ForeignKey(Post, models.SET_NULL, null=True)
-#     liked = models.BooleanField(models.SET_DEFAULT())
+class Like(models.Model):
+    user = models.ForeignKey(User, models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, models.SET_NULL, null=True)
+    liked = models.BooleanField(default=False)
 
-#     def __str__(self):
-#         return f"{self.id}"
+    def __str__(self):
+        return f"{self.id}"
 
+class Follow(models.Model):
+    user = models.OneToOneField(User, models.SET_NULL, null=True)
+    following = models.ManyToManyField(User, blank=True, related_name="followers")
 
-# class Follow(models.Model):
-#     user = models.OneToOneField(User, models.SET_NULL, null=True, related_name="user_follow_list", unique=True)
-#     follower = models.ManyToManyField(User, blank=True, related_name="follower")
-
-#     def __str__(self):
-#         return f"{self.id}"
+    def __str__(self):
+        return f"{self.id}"
