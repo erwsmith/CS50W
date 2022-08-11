c62923eaeb6650b9a18a70f8ad947e2ce8e9ca7c
Eric Smith <erwsmith@gmail.com>
2022-08-11 11:15:09 -0600

    add following and profile

--- w7-testing-ci-cd/project4/network/urls.py
+++ w7-testing-ci-cd/project4/network/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
+    path("following/<int:user_id>/", views.following, name="following"),
+    path("profile/<int:user_id>/", views.profile, name="profile"),
+
 ]
