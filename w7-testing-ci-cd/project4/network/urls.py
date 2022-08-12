eb448f77794945b05265d39237244eea26b15874
Eric Smith <erwsmith@gmail.com>
2022-08-12 11:38:04 -0600

    rm int:user_id from following_view path

--- w7-testing-ci-cd/project4/network/urls.py
+++ w7-testing-ci-cd/project4/network/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path("<int:user_id>/following/", views.following_view, name="following_view"),
+    path("following_view/", views.following_view, name="following_view"),
     path("<int:user_id>/profile/", views.profile, name="profile"),
 
 ]
