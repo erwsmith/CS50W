257c49ca5801320826c074bf22d45ebf17a5419e
Eric Smith <erwsmith@gmail.com>
2022-08-16 15:26:49 -0600

    add API routes

--- w7-testing-ci-cd/project4/network/urls.py
+++ w7-testing-ci-cd/project4/network/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("following_view/", views.following_view, name="following_view"),
     path("<int:user_id>/profile/", views.profile, name="profile"),
+
+    # API Routes
+    path("posts/<str:filter>", views.filtered_posts, name="filtered_posts"),
+    path("posts/<int:post_id>", views.post_view, name="post_view")
 ]
