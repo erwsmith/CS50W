cdcb11f8efbfe42c00084ff1aa1ccdf3bee41ecc
Eric Smith <erwsmith@gmail.com>
2022-08-16 15:27:23 -0600

    some working things

--- w7-testing-ci-cd/project4/network/static/network/network.js
+++ w7-testing-ci-cd/project4/network/static/network/network.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#filtered-posts').addEventListener('click', () => load_posts('all'));
     document.querySelectorAll('input').forEach(button => {
         button.onclick = function() {
             console.log(`${this.dataset.name} button clicked!`)
+            if (this.dataset.name === "edit") {
+                console.log("EDIT BUTTON")
+            }
         };
     });
   });
 
 
-// function load_display(display) {
-//     // Show create view and hide other views
-//     document.querySelector('#posts-view').style.display = 'block';
+function load_posts(filter) {
+    // Show create view and hide other views
+    document.querySelector('#posts-view-js').style.display = 'block';
+    fetch(`/posts/${filter}`)
+    .then(response => response.json())
+    .then(posts => {
+        posts.forEach((post) => {
+            const post_data = [`${post.id}`, `${post.username}`, `${post.body}`, `${post.timestamp}`]
+        });
+    });
+}
 
-//     fetch(`/posts/${display}`)
-//     .then(response => response.json())
-//     .then(posts => {
-//         console.log(posts)
-//         // posts.forEach((post) => {
-//         //     const post_data = [`${post.user}`]
-//         // });
-//     });
-// }
+function load_post(post) {
+    fetch(`posts/${post.id}`)
+    .then(response = response.json())
+    .then(console.log(`${post}`));
+}
