fa08d47e24b14972ee08104c1369c4c68be2bfee
Eric Smith <erwsmith@gmail.com>
2022-08-17 12:25:19 -0600

    add button specific functions

--- w7-testing-ci-cd/project4/network/static/network/network.js
+++ w7-testing-ci-cd/project4/network/static/network/network.js
@@ -4,30 +4,55 @@ document.addEventListener('DOMContentLoaded', function() {
     // document.querySelector('#profile-posts').addEventListener('click', () => load_posts('profile'));
     document.querySelectorAll('input').forEach(button => {
         button.onclick = function() {
-                console.log(`${this.dataset.name} button ${this.dataset.postid} clicked!`);
-                const post_id = parseInt(this.dataset.postid);
-                load_post(post_id);
+                post_id = this.dataset.postid
+                button_name = this.dataset.name
+                if (button_name === "edit") {
+                    edit_post(post_id)
+                } else if (button_name === "like") {
+                    console.log(`${button_name} button ${post_id} clicked!`)
+                    like_post(post_id)
+                } else if (button_name === "unlike") {
+                    console.log(`${button_name} button ${post_id} clicked!`)
+                    unlike_post(post_id)
+                }
             }
-        });
-    });
+        })
+    })
 
 function load_posts(filter) {
-    // Show create view and hide other views
     document.querySelector('#posts-view-js').style.display = 'block';
     fetch(`/posts/${filter}`)
     .then(response => response.json())
     .then(posts => {
         posts.forEach((post) => {
             const post_data = [`${post.id}`, `${post.username}`, `${post.body}`, `${post.timestamp}`]
-        });
-    });
+        })
+    })
 }
 
-function load_post(post_id) {
+function like_post(post_id) {
+    fetch(`posts/${post_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            liked: true // this isn't correct
+        })
+    })
+}
+
+function unlike_post(post_id) {
+    fetch(`posts/${post_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            liked: false // this isn't correct
+        })
+    })
+}
+
+function edit_post(post_id) {
     fetch(`posts/${post_id}`)
     .then(response => response.json())
     .then(post => {
-        const post_data = [`${post.id}`, `${post.username}`, `${post.body}`, `${post.timestamp}`];
-        console.log(post_data);
-    });
+        const post_data = [`${post.id}`, `${post.username}`, `${post.body}`, `${post.timestamp}`]
+        console.log(post_data)
+    })
 }
