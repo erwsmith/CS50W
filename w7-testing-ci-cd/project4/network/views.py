fccfdc449d6aa6cba043d623b174440fb14f33c4
Eric Smith <erwsmith@gmail.com>
2022-09-22 11:42:56 -0600

    add filtered_posts view initial

--- w7-testing-ci-cd/project4/network/views.py
+++ w7-testing-ci-cd/project4/network/views.py
@@ -37,6 +37,28 @@ def index(request):
     })
 
 
+def filtered_posts(request, post_view):
+    # Filter emails returned based on filter
+    if post_view == "profile":
+        emails = Post.objects.filter(
+            user=request.user,
+        )
+    elif post_view == "all":
+        emails = Post.objects.filter(
+            user=request.user,
+        )
+    elif post_view == "following":
+        emails = Post.objects.filter(
+            user=request.user,
+        )
+    else:
+        return JsonResponse({"error": "Invalid filter."}, status=400)
+
+    # Return emails in reverse chronologial order
+    emails = emails.order_by("-timestamp").all()
+    return JsonResponse([email.serialize() for email in emails], safe=False)
+
+
 def following_view(request):
     if request.method == "POST":
         form = CreatePostForm(request.POST)
