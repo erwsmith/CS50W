5662470dd0dd85f5d02a4629e04e183cb8e1962c
Eric Smith <erwsmith@gmail.com>
2022-09-22 11:42:42 -0600

    rm some paths, switching to js version

--- w7-testing-ci-cd/project4/network/urls.py
+++ w7-testing-ci-cd/project4/network/urls.py
@@ -1,6 +1,4 @@
-
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
@@ -8,10 +6,7 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path("following_view/", views.following_view, name="following_view"),
-    path("<int:user_id>/profile/", views.profile, name="profile"),
 
     # API Routes
-    # path("posts/<str:filter>", views.filtered_posts, name="filtered_posts"),
-    path("posts/<int:post_id>", views.post_view, name="post_view")
+    path("posts/<str:filter>", views.filtered_posts, name="filtered_posts"),
 ]
