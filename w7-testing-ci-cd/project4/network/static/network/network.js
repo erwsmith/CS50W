7d2df5d98a01ac8a90b2d22161225a6f0c592aa1
Eric Smith <erwsmith@gmail.com>
2022-09-23 10:48:33 -0600

    add click functionality to cells in load_posts()

--- w7-testing-ci-cd/project4/network/static/network/network.js
+++ w7-testing-ci-cd/project4/network/static/network/network.js
@@ -4,34 +4,40 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#profile-posts').addEventListener('click', () => load_posts('profile'));
     document.querySelectorAll('input').forEach(button => {
         button.onclick = function() {
-                post_id = this.dataset.postid
-                active_user_id = this.dataset.active_user_id
-                button_name = this.dataset.name
-                if (button_name === "edit") {
-                    edit_post(post_id, active_user_id)
-                } else if (button_name === "like") {
-                    console.log(`${button_name} button ${post_id} clicked!`)
-                    like_post(post_id, active_user_id)
-                } else if (button_name === "unlike") {
-                    console.log(`${button_name} button ${post_id} clicked!`)
-                    unlike_post(post_id, active_user_id)
-                }
+            post_id = this.dataset.postid
+            active_user_id = this.dataset.active_user_id
+            button_name = this.dataset.name
+            if (button_name === "edit") {
+                edit_post(post_id, active_user_id)
+            } else if (button_name === "like") {
+                console.log(`${button_name} button ${post_id} clicked!`)
+                like_post(post_id, active_user_id)
+            } else if (button_name === "unlike") {
+                console.log(`${button_name} button ${post_id} clicked!`)
+                unlike_post(post_id, active_user_id)
             }
-        })
+        }
     })
+})
 
 
 function load_posts(post_view) {
+    document.querySelector('#grid').innerHTML = '';
     document.querySelector('#posts-view').style.display = 'block';
+    console.log(post_view)
     fetch(`/posts/${post_view}`)
     .then(response => response.json())
     .then(posts => {
         posts.forEach((post) => {
-            const post_data = [`${post.user}`, `${post.body}`, `${post.timestamp}`]
+            const post_data = [`${post.id}`, `${post.username}`, `${post.body}`, `${post.timestamp}`]
             const grid = document.getElementById("grid");
             for (let a of post_data) { 
                 let cell = document.createElement("div");
                 cell.innerHTML = a;
+                cell.addEventListener('click', function() {
+                    console.log(`User ${post.username} has been clicked!`);
+                    // load_email(email);
+                  });
                 grid.appendChild(cell);
             }
         });
