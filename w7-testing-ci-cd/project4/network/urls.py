5974805d659b8404933a87c61b2ff5f1d1eb248a
Eric Smith <erwsmith@gmail.com>
2022-09-27 10:32:27 -0600

    more robust, intuitive paths (differentiated them)

--- w7-testing-ci-cd/project4/network/urls.py
+++ w7-testing-ci-cd/project4/network/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("register/", views.register, name="register"),
 
     # API Routes
-    path("posts/<int:post_view>", views.filtered_posts, name="filtered_posts"),
-    path("posts/<str:username>", views.profile_view, name="profile_view"),
+    path("posts/<str:post_view>", views.filtered_posts, name="filtered_posts"),
+    path("profile/<str:username>", views.profile_view, name="profile_view"),
 ]
