6fa74b880b6ac1b91155eed3814af08155c6f654
Eric Smith <erwsmith@gmail.com>
2022-09-28 14:25:18 -0600

    remove post_cell, copy code to load_profile()

--- w7-testing-ci-cd/project4/network/static/network/network.js
+++ w7-testing-ci-cd/project4/network/static/network/network.js
@@ -48,16 +48,12 @@ function load_posts(post_view) {
     .then(posts => {
         posts.forEach((post) => {
             
-            let post_cell = document.createElement('div')
-            post_cell.id = "post_cell"
-            post_cell.className = "post_cell"
-            
             let post_username = document.createElement('button')
             post_username.innerHTML = `${post.username}`
             post_username.id = "post_username"
             post_username.className = "btn btn-link m-0 p-0"
             post_username.addEventListener('click', function() {
-                console.log(`User ${post.username} clicked!`)
+                load_profile(post.username)
             });
             
             let post_body = document.createElement('div')
@@ -86,26 +82,79 @@ function load_posts(post_view) {
             like_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
             like_button.addEventListener('click', function() {
                 console.log(`Like button ${post.id} clicked!`)
-                
             });
 
             let likes_count = document.createElement('span')
             likes_count.className = "mx-2"
             likes_count.innerHTML = `${post.likes_count}`
 
-            document.querySelector('#posts-view').append(post_username, post_body, post_timestamp, edit_button, like_button, likes_count, document.createElement('hr'))
+            document.querySelector('#posts-view').append(
+                document.createElement('hr'), 
+                post_username, 
+                post_body, 
+                post_timestamp, 
+                edit_button, 
+                like_button, 
+                likes_count, 
+                )
         });
     });
 }
 
 function load_profile(username) {
+    document.querySelector('#posts-view-head').innerHTML = `<h3>${username}'s Profile</h3>`;
     document.querySelector('#posts-view').innerHTML = '';
     document.querySelector('#posts-view').style.display = 'block';
     fetch(`/profile/${username}`)
     .then(response => response.json())
     .then(posts => {
         posts.forEach((post) => {
+            let post_username = document.createElement('div')
+            post_username.innerHTML = `${post.username}`
+            post_username.id = "post_username"
+            post_username.className = "m-0 p-0"
             
+            let post_body = document.createElement('div')
+            post_body.className = "m-2"
+            post_body.innerHTML = `${post.body}`
+            
+            let post_timestamp = document.createElement('div')
+            post_timestamp.className = "text-muted m-2"
+            post_timestamp.innerHTML = `${post.timestamp}`
+            
+            let edit_button = document.createElement('button');
+            if (post.user_id === parseInt(active_user_id)) {
+                edit_button.innerHTML = "Edit";
+                edit_button.id = "edit";
+                edit_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
+                edit_button.addEventListener('click', function() {
+                    console.log(`Edit button ${post.id} clicked!`)
+                });
+            } else {
+                edit_button.style.display = "none";
+            }
+
+            let like_button = document.createElement('button');
+            like_button.innerHTML = "Like";
+            like_button.id = "like";
+            like_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
+            like_button.addEventListener('click', function() {
+                console.log(`Like button ${post.id} clicked!`)
+            });
+
+            let likes_count = document.createElement('span')
+            likes_count.className = "mx-2"
+            likes_count.innerHTML = `${post.likes_count}`
+
+            document.querySelector('#posts-view').append(
+                document.createElement('hr'), 
+                post_username, 
+                post_body, 
+                post_timestamp, 
+                edit_button, 
+                like_button, 
+                likes_count, 
+                )
             });
     });
 }
