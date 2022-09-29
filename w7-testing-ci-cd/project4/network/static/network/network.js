4dadcc4bc32c08ccab0be960509da864b61cebc6
Eric Smith <erwsmith@gmail.com>
2022-09-29 15:30:00 -0600

    combined load_profile() into load_posts(), working

--- w7-testing-ci-cd/project4/network/static/network/network.js
+++ w7-testing-ci-cd/project4/network/static/network/network.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 
-function load_posts(post_view) {
+function load_posts(post_view, username='none') {
+    
     let view_name = ''
     if (post_view === 'all') {
         view_name = "All Posts";
@@ -18,6 +19,7 @@ function load_posts(post_view) {
         console.log("Error: Invalid Post View")
         view_name = "All Posts";
     }
+
     document.querySelector('#posts-view-head').innerHTML = `<h3>${view_name}</h3>`;
     document.querySelector('#posts-view').style.display = 'block';
     document.querySelector('#posts-view').innerHTML = '';
@@ -25,8 +27,13 @@ function load_posts(post_view) {
     let au = document.querySelector('#active_user_id')
     let active_user_id = au.dataset.active_user_id
     
+    if (username == 'none') {
+        endpoint = `/posts/${post_view}`
+    } else {
+        endpoint = `/profile/${username}`
+    }
 
-    fetch(`/posts/${post_view}`)
+    fetch(endpoint)
     .then(response => response.json())
     .then(posts => {
         posts.forEach((post) => {
@@ -36,7 +43,7 @@ function load_posts(post_view) {
             post_username.id = "post_username"
             post_username.className = "btn btn-link m-0 p-0"
             post_username.addEventListener('click', function() {
-                load_profile(post.username)
+                load_posts('profile', post.username)
             });
             
             let post_body = document.createElement('div')
@@ -81,65 +88,6 @@ function load_posts(post_view) {
                 likes_count, 
                 )
         });
-    });
-}
-
-function load_profile(username) {
-    document.querySelector('#posts-view-head').innerHTML = `<h3>${username}'s Profile</h3>`;
-    document.querySelector('#posts-view').innerHTML = '';
-    document.querySelector('#posts-view').style.display = 'block';
-    fetch(`/profile/${username}`)
-    .then(response => response.json())
-    .then(posts => {
-        posts.forEach((post) => {
-            let post_username = document.createElement('div')
-            post_username.innerHTML = `${post.username}`
-            post_username.id = "post_username"
-            post_username.className = "m-0 p-0"
-            
-            let post_body = document.createElement('div')
-            post_body.className = "m-2"
-            post_body.innerHTML = `${post.body}`
-            
-            let post_timestamp = document.createElement('div')
-            post_timestamp.className = "text-muted m-2"
-            post_timestamp.innerHTML = `${post.timestamp}`
-            
-            let edit_button = document.createElement('button');
-            if (post.user_id === parseInt(active_user_id)) {
-                edit_button.innerHTML = "Edit";
-                edit_button.id = "edit";
-                edit_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
-                edit_button.addEventListener('click', function() {
-                    console.log(`Edit button ${post.id} clicked!`)
-                });
-            } else {
-                edit_button.style.display = "none";
-            }
-
-            let likes_count = document.createElement('span')
-            likes_count.className = "mx-2"
-            likes_count.innerHTML = `${post.likes_count}`
-
-            let like_button = document.createElement('button');
-            like_button.innerHTML = "Like";
-            like_button.id = "like_button";
-            like_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
-            like_button.addEventListener('click', function() {
-                like_post(post.id, active_user_id, post_view)
-                document.querySelector('#likes_count').innerHTML = `${post.likes_count}`
-            });
-
-            document.querySelector('#posts-view').append(
-                document.createElement('hr'), 
-                post_username, 
-                post_body, 
-                post_timestamp, 
-                edit_button, 
-                like_button, 
-                likes_count, 
-                )
-            });
     });
 }
 
