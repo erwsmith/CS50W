b49bdf4267f90c90c4eddfa0b01067c5c93aca33
Eric Smith <erwsmith@gmail.com>
2022-09-30 13:37:05 -0600

    working following/follower count display

--- w7-testing-ci-cd/project4/network/static/network/network.js
+++ w7-testing-ci-cd/project4/network/static/network/network.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded',() => {
-    document.querySelector('#all-posts').addEventListener('click', () => load_posts('all'));
-    document.querySelector('#following-posts').addEventListener('click', () => load_posts('following'));
-    document.querySelector('#profile-posts').addEventListener('click', () => load_posts('profile'));
-    load_posts('all');
+    document.querySelector('#all-posts').addEventListener('click', () => show_posts('all'));
+    document.querySelector('#following-posts').addEventListener('click', () => show_posts('following'));
+    document.querySelector('#profile-posts').addEventListener('click', () => show_posts('profile'));
+    show_posts('all');
 })
 
 
-function load_posts(post_view, username='none') {
+function show_posts(post_view, username='none') {
     
     let view_name = ''
     if (post_view === 'all') {
@@ -36,68 +36,7 @@ function load_posts(post_view, username='none') {
         fetch(`/posts/${post_view}`)
         .then(response => response.json())
         .then(posts => {
-            posts.forEach(post => {
-
-                // all_post_data = [`
-                //     ${post.id}, 
-                //     ${post.username}, 
-                //     ${post.user_id}, 
-                //     ${post.body}, 
-                //     ${post.timestamp}, 
-                //     ${post.liked_by}, 
-                //     ${post.likes_count}
-                // `]
-                
-                let post_username = document.createElement('button')
-                post_username.innerHTML = `${post.username}`
-                post_username.id = "post_username"
-                post_username.className = "btn btn-link m-0 p-0"
-                post_username.addEventListener('click', () => {
-                    load_posts('profile', post.username)
-                });
-                
-                let post_body = document.createElement('div')
-                post_body.className = "m-2"
-                post_body.innerHTML = `${post.body}`
-                
-                let post_timestamp = document.createElement('div')
-                post_timestamp.className = "text-muted m-2"
-                post_timestamp.innerHTML = `${post.timestamp}`
-                
-                let edit_button = document.createElement('button');
-                if (post.user_id === parseInt(active_user_id)) {
-                    edit_button.innerHTML = "Edit";
-                    edit_button.id = "edit";
-                    edit_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
-                    edit_button.addEventListener('click', () => {
-                        console.log(`Edit button ${post.id} clicked!`)
-                    });
-                } else {
-                    edit_button.style.display = "none";
-                }
-
-                let likes_count = document.createElement('span')
-                likes_count.className = "mx-2"
-                likes_count.innerHTML = `${post.likes_count}`
-
-                let like_button = document.createElement('button');
-                like_button.innerHTML = "Like";
-                like_button.id = "like";
-                like_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
-                like_button.addEventListener('click', () => {
-                    like_post(post.id, active_user_id, post_view)
-                })
-
-                document.querySelector('#posts-view').append(
-                    document.createElement('hr'), 
-                    post_username, 
-                    post_body, 
-                    post_timestamp, 
-                    edit_button, 
-                    like_button, 
-                    likes_count, 
-                    )
-            });
+            get_post_data(posts)
         });
     } else {
         Promise.all([
@@ -110,9 +49,33 @@ function load_posts(post_view, username='none') {
             }));
         }).then((data) => {
             let posts = data[0]
-            console.log(posts);
-            let follower_data = data[1]
-            console.log(`Followers: ${follower_data.followers_count}, Following: ${follower_data.following_count}`);
+            get_post_data(posts)
+            
+            let follower = data[1]
+
+            let following_count = document.createElement('span')
+            following_count.className = "h4 ml-2 font-weight-bold"
+            following_count.innerHTML = `${follower.following_count}`
+
+            let following_text = document.createElement('span')
+            following_text.innerHTML = "Following"
+            following_text.className = "my-2 ml-2 mr-4"
+
+            let followers_count = document.createElement('span')
+            followers_count.className = "h4 ml-2 font-weight-bold"
+            followers_count.innerHTML = `${follower.followers_count}`
+
+            let followers_text = document.createElement('span')
+            followers_text.innerHTML = "Follower(s)"
+            followers_text.className = "m-2"
+
+            document.querySelector('#posts-view-head').append(
+                following_count,
+                following_text,
+                followers_count,
+                followers_text,
+                
+                )
         }).catch((error) => {
                     console.log(error);
                 });
@@ -137,7 +100,71 @@ function load_posts(post_view, username='none') {
 
 }
 
+function get_post_data(posts) {
 
+    posts.forEach(post => {
+
+        // all_post_data = [`
+        //     ${post.id}, 
+        //     ${post.username}, 
+        //     ${post.user_id}, 
+        //     ${post.body}, 
+        //     ${post.timestamp}, 
+        //     ${post.liked_by}, 
+        //     ${post.likes_count}
+        // `]
+        
+        let post_username = document.createElement('button')
+        post_username.innerHTML = `${post.username}`
+        post_username.id = "post_username"
+        post_username.className = "btn btn-link m-0 p-0"
+        post_username.addEventListener('click', () => {
+            show_posts('profile', post.username)
+        });
+        
+        let post_body = document.createElement('div')
+        post_body.className = "m-2"
+        post_body.innerHTML = `${post.body}`
+        
+        let post_timestamp = document.createElement('div')
+        post_timestamp.className = "text-muted m-2"
+        post_timestamp.innerHTML = `${post.timestamp}`
+        
+        let edit_button = document.createElement('button');
+        if (post.user_id === parseInt(active_user_id)) {
+            edit_button.innerHTML = "Edit";
+            edit_button.id = "edit";
+            edit_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
+            edit_button.addEventListener('click', () => {
+                console.log(`Edit button ${post.id} clicked!`)
+            });
+        } else {
+            edit_button.style.display = "none";
+        }
+
+        let likes_count = document.createElement('span')
+        likes_count.className = "mx-2"
+        likes_count.innerHTML = `${post.likes_count}`
+
+        let like_button = document.createElement('button');
+        like_button.innerHTML = "Like";
+        like_button.id = "like";
+        like_button.className = "btn btn-sm btn-outline-dark mx-2 px-3";
+        like_button.addEventListener('click', () => {
+            like_post(post.id, active_user_id, post_view)
+        })
+
+        document.querySelector('#posts-view').append(
+            document.createElement('hr'), 
+            post_username, 
+            post_body, 
+            post_timestamp, 
+            edit_button, 
+            like_button, 
+            likes_count, 
+            )
+    });
+}
 
 
 function like_post(post_id, active_user_id, post_view) {
@@ -150,7 +177,7 @@ function like_post(post_id, active_user_id, post_view) {
     .then(function() {document.location.reload()});
     
     // TODO - update likes count without reloading the whole page, don't change scroll position
-    // .then(function() {load_posts(post_view);});
+    // .then(function() {show_posts(post_view);});
     // .then(function () {
     //     fetch(`/like_post/${post_id}`)
     //     .then(response => response.json())
@@ -183,7 +210,6 @@ function like_post(post_id, active_user_id, post_view) {
 //         })
 //     })
 // }
-
 
 // function edit_post(post_id, active_user_id) {
 //     fetch(`/posts/${post_id}`)
