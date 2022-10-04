8afa7bd77146856bbcc4795a72da7481225c8e8a
Eric Smith <erwsmith@gmail.com>
2022-10-03 19:28:33 -0600

    move like_post to API routes section

--- w7-testing-ci-cd/project4/network/urls.py
+++ w7-testing-ci-cd/project4/network/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path("like_post/<int:post_id>", views.like_post, name="like_post"),
 
     # API Routes
     path("posts/<str:post_view>", views.filtered_posts, name="filtered_posts"),
     path("profile/<str:username>", views.profile_view, name="profile_view"),
     path("followers/<str:username>", views.get_followers, name="get_followers"),
+    path("like_post/<int:post_id>", views.like_post, name="like_post"),
 ]
