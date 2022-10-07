fb31a188cfec6ff38ce436010977a8cbb486110d
Eric Smith <erwsmith@gmail.com>
2022-10-07 12:53:42 -0600

    add some pagination stuff

--- w7-testing-ci-cd/project4/network/views.py
+++ w7-testing-ci-cd/project4/network/views.py
@@ -5,7 +5,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
