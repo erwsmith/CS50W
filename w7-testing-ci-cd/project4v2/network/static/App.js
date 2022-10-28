1ee5bbbc247325b675a23ec98b39529a1154c695
Eric Smith <erwsmith@gmail.com>
2022-10-28 12:39:13 -0600

    attempt to connect react app with django app

---
+++ w7-testing-ci-cd/project4v2/network/static/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  console.log('App called');
+  
+  return (
+    <div className="container">
+      <h2>App</h2>
+    </div>
+  );
+};
+
+export default App;
