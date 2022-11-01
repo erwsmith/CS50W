7fb6211f7d18a47e7b9eb25915d24ee9b05323f9
Eric Smith <erwsmith@gmail.com>
2022-11-01 12:36:07 -0600

    add Search

--- w7-testing-ci-cd/project4v3/project4/frontend/src/App.js
+++ w7-testing-ci-cd/project4v3/project4/frontend/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Search from './Search';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <div className='container'><Search /></div>
   );
 }
 
